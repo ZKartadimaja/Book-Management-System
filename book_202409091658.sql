b29e27ee88443993b68b01f8db67fdaaaf65762b
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-09 16:59:30 +0700

    EXPORT DATA

---
+++ book_202409091658.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.book (title,isbn,author_id,genre_id,published_date) VALUES
+	 ('Laskar Pelangi','ISBN0101',1,1,'12-02-2022'),
+	 ('Laskar Pelangi 2','ISBN0202',1,1,'13-02-2023'),
+	 ('Sherlock Homes','ISBN0303',3,2,'11-01-2021'),
+	 ('Sherlock Homes 2','ISBN0404',3,2,'09-01-2023'),
+	 ('Upin dan Ipin','ISBN0505',5,2,'15-10-2022'),
+	 ('Budi Manusia Api','ISBN0606',6,3,'16-08-2022'),
+	 ('Panji Petualang','ISBN0707',9,1,'25-03-2019'),
+	 ('Avatar','ISBN0809',10,4,'26-04-2020'),
+	 ('Marsha','ISBN0909',6,3,'21-04-2018'),
+	 ('Krishna','ISBN0001',2,5,'19-07-2012');
---
+++ genre_202409091658.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.genre ("name") VALUES
+	 ('Action'),
+	 ('Fiction'),
+	 ('Non-Fiction'),
+	 ('Fable'),
+	 ('Mystery'),
+	 ('Dongeng'),
+	 ('Sci-Fi'),
+	 ('Kiri'),
+	 ('Kanan'),
+	 ('Politik');
