700efa0968d1afd8f6872f078c2d28da899aa218
ZKartadimaja <zulfikarzamzam13@gmail.com>
2024-09-09 17:14:36 +0700

    AuthorController

--- my-book-management-system/src/main/java/com/example/my_book_management_system/service/AuthorService.java
+++ my-book-management-system/src/main/java/com/example/my_book_management_system/service/AuthorService.java
@@ -1,4 +1,16 @@
 package com.example.my_book_management_system.service;
 
+import com.example.my_book_management_system.entity.AuthorEntity;
+import com.example.my_book_management_system.entity.GenreEntity;
+
+import java.util.List;
+
 public interface AuthorService {
+    List<AuthorEntity> getAllAuthor();
+
+    AuthorEntity getAuthorById(Long id);
+
+    GenreEntity deleteAuthor(AuthorEntity author);
+
+    AuthorEntity saveAuthor(AuthorEntity author);
 }
